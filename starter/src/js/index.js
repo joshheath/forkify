6d9aa8e8fa4b0f7bc87c3407a6473132aa3fcac6
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-01-22 12:16:17 +0000

    add api callback function from food2fork

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,1 +1,18 @@
-// Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const key = '8be3440ce9c8520a097af6981a258d04';
+
+  try{
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes
+    console.log(recipes);
+  } catch (error){
+    alert(error);
+  }
+
+}
+
+getResults('souffle');
