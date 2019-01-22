8a464f9f4a70da25ef779dd059fbda6f3dba80c7
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-01-22 12:45:28 +0000

    add async function to search.js

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,18 +1,4 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = '8be3440ce9c8520a097af6981a258d04';
-
-  try{
-    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes
-    console.log(recipes);
-  } catch (error){
-    alert(error);
-  }
-
-}
-
-getResults('souffle');
+const search = new Search('souffle');
+console.log(search);
--- starter/src/js/models/Search.js
+++ starter/src/js/models/Search.js
@@ -1,1 +1,21 @@
-export default 'i am an exported string';
+import axios from 'axios';
+
+export default class Search {
+  constructor(query){
+    this.query = query;
+  }
+
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '8be3440ce9c8520a097af6981a258d04';
+
+    try{
+      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+
+      this.result = res.data.recipes
+      console.log(this.result);
+    } catch (error){
+      alert(error);
+    }
+  }
+}
