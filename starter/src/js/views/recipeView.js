7183821a121e01e225e3d159714bb25a84a7e531
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-02-07 09:20:21 +0000

    small error fix to get recipe images displaying again

--- starter/src/js/views/recipeView.js
+++ starter/src/js/views/recipeView.js
@@ -39,7 +39,7 @@ const createIngredient = ingredient => `
 export const renderRecipe = recipe => {
   const markup = `
       <figure class="recipe__fig">
-          <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
+          <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img">
           <h1 class="recipe__title">
               <span>${recipe.title}</span>
           </h1>
