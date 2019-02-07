28e9e25aaef98f72a2054deee5979b5af78e34bf
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-02-07 15:04:45 +0000

    add renderItem to List view

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -74,7 +75,6 @@ const controlRecipe = async () => {
     try {
       // get recipe data and parse ingredients
       await state.recipe.getRecipe();
-      console.log(state.recipe.ingredients);
       state.recipe.parseIngredients();
 
       // calculate servings and time
@@ -108,8 +108,12 @@ elements.recipe.addEventListener('click', e=> {
     recipeView.updateServingsIngredients(state.recipe);
 
   }
-  console.log(state.recipe);
 });
+
+window.l = new List();
+
+
+
 
 
 
--- starter/src/js/models/List.js
+++ starter/src/js/models/List.js
@@ -12,11 +12,18 @@ export default class List {
       unit,
       ingredient
     }
+    this.items.push(item);
+    return item;
   }
 
   deleteItem(id){
     const index = this.items.findIndex(el => el.id ===id);
 
     this.items.splice(index, 1);
+  }
+
+  updateCount(id, newCount) {
+    // loops through all elements in the items array and returns the item with the id that is passed into the function. the object is returned and the count increased
+    this.items.find(el => el.id === id).count = newCount;
   }
 }
