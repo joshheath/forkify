af96e7ae2d6cc4657c60b1f4a898df8db5d58642
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-02-27 13:45:07 +0000

    add method to read likes from localstorage back into state

--- starter/src/js/models/Likes.js
+++ starter/src/js/models/Likes.js
@@ -32,4 +32,11 @@ export default class Likes {
   persistData(){
     localStorage.setItem('likes', JSON.stringify(this.likes));
   }
+
+  readStorage(){
+    const storage = JSON.parse(localStorage.getItem('likes'));
+
+    // reloading the likes list from localstorage
+    if (storage) this.likes = storage;
+  }
 }
