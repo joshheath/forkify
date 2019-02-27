ce5e037c6ed701b23dedd2aa2a172c81d35ede28
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-02-27 13:38:54 +0000

    add persistData method to store likes in localstorage

--- starter/src/js/models/Likes.js
+++ starter/src/js/models/Likes.js
@@ -6,12 +6,18 @@ export default class Likes {
   addLike(id, title, author, img) {
     const like = { id, title, author, img };
     this.likes.push(like);
+
+    // persist the data in localstorage
+    this.persistData();
     return like;
   }
 
   deleteLike(id) {
     const index = this.likes.findIndex(el => el.id === id);
     this.likes.splice(index, 1);
+
+    // persist the data in localstorage
+    this.persistData();
   }
 
   isLiked(id){
@@ -21,5 +27,9 @@ export default class Likes {
 
   getNumLikes(){
     return this.likes.length;
+  }
+
+  persistData(){
+    localStorage.setItem('likes', JSON.stringify(this.likes));
   }
 }
